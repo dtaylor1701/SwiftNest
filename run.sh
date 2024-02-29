a7953ff1a85413436f03bb09672f87cc6017c0b3
David Taylor <dktaylor1701@gmail.com>
2024-02-29 00:12:57 -0500

    Added BlueJay, Twigs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 rm -rf swift-package-collection-generator
-git clone git@github.com:apple/swift-package-collection-generator.git
+git clone git@github.com:dtaylor1701/swift-package-collection-generator.git
 swift run --package-path swift-package-collection-generator package-collection-generate packages.json collection.json
